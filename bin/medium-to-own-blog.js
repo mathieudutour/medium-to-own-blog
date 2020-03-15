2e19e8ef7c65d85cc0a28524b8e5d15ad6869f73
Mathieu Dutour <mathieu@dutour.me>
2020-03-15 12:37:09 +0100

    Update medium-to-own-blog.js

--- bin/medium-to-own-blog.js
+++ bin/medium-to-own-blog.js
@@ -92,7 +92,7 @@ Now let's work on putting it online.
 GitHub is a free platform to host code online.
 
 1. Head over https://github.com/new and create a new repository. 
-   Don't initialize a license or readme or you will encounter issues in the next step.
+   Do NOT create it with a license or readme - otherwise issues  will arise later.
 2. Copy paste the URL of your new repository here
 `)
     return inquirer.prompt([
